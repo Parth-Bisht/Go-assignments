ecbea1282fe869f823558425a81ee6c89f83fc1d
Parth Bisht <bishtparth795@gmail.com>
2023-07-04 11:21:03 +0530

    typeOf

--- booking-app/main.go
+++ booking-app/main.go
@@ -3,18 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "GO Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	var conferenceName string = "GO Conference"
+	const conferenceTickets int = 50
+	var remainingTickets int = 50
+
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
-	fmt.Println("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName
+	var userName string
+	var userTickets int
 	// ask user for their name
 
-	userNmae = "Tom"
-	fmt.Println(userName)
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
 
 }
