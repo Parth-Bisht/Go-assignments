48f08ad5def78538e9caa566e43d0b0ec6c35d39
Parth Bisht <bishtparth795@gmail.com>
2023-07-04 11:32:26 +0530

    user input

--- booking-app/main.go
+++ booking-app/main.go
@@ -3,22 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName string = "GO Conference"
+	conferenceName := "GO Conference"
 	const conferenceTickets int = 50
 	var remainingTickets int = 50
 
-	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
+	// fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
 	// ask user for their name
+	fmt.Println("Enter your firstName: ")
+	fmt.Scan(&firstName)
 
-	userName = "Tom"
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+	fmt.Println("Enter your lastName: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will recieve a confirmation email at %v", firstName, lastName, userTickets, email)
 
 }
