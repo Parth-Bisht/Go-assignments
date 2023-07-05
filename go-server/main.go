e82cf89c04a64997ccb0df8855cc1c219ff5c707
Parth Bisht <bishtparth795@gmail.com>
2023-07-05 13:17:39 +0530

    Basic go-server

--- go-server/main.go
+++ go-server/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "hello" {
+	if r.URL.Path != "/hello" {
 		http.Error(w, "404 not found", http.StatusNotFound)
 		return
 	}
