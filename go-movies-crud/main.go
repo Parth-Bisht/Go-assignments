eb486be0f01e06c341e90a24ba610e3d9307b03d
Parth Bisht <bishtparth795@gmail.com>
2023-07-10 17:42:34 +0530

    go-movies-crud

---
+++ go-movies-crud/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"encoding/json"
+	"math/rand"
+	"strconv"
+	"github.com/gorilla/mux"
+)
+
+type Movie struct{
+	ID string `json:"id`
+	Isbn string `json:"isbn"`
+	Title string `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct{
+	Firstname string `json:"firstname"`
+	Lastname string `json:"lastname"`
+}
