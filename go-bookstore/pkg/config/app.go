77723929c5aa25bd7a6c60ae4fa1053e9023d205
Parth Bisht <bishtparth795@gmail.com>
2023-07-12 16:50:52 +0530

    testing

--- go-bookstore/pkg/config/app.go
+++ go-bookstore/pkg/config/app.go
@@ -10,7 +10,7 @@ var (
 )
 
 func Connect(){
-	d, err := gorm.Open("mysql", "parth:Parth@2000/simplerest?charset=utf8&parseTime=True&loc=Local")
+	d, err := gorm.Open("mysql", "parth:Parth@2000@/simplerest?charset=utf8&parseTime=True&loc=Local")
 	if err != nil{
 		panic(err)
 	} 
